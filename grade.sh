edc865025d2a30b5441de60f6b36909b65d9c3ad
ambermtse <atse@ucsd.edu>
2023-11-28 13:01:48 -0800

    lab8

--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -8,6 +9,23 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+
+if ! test -f student-submission/ListExamples.java
+    then echo "ListExamples.java file not found"
+    exit
+fi
+
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+cd grading-area
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples 
+
+if ! test $? -eq 0 
+then echo "Not all tests passed"
+else echo "All tests passed"
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
